d31a0c446722e2b7de51d0b837c3e30eead6e8be
Jeet-bhardwaj <bhardwajjeet409@gmail.com>
2024-09-04 11:23:48 +0530

    Successfully conected to DataBase

--- Backend/db.js
+++ Backend/db.js
@@ -1,3 +1,14 @@
-// getting-started.js
 const mongoose = require('mongoose');
-mongoose.connect('')
+const mongoURL = 'mongodb+srv://bhardwajjeet408:2b8ykuIE0PsH9O56@cluster0.f1pcg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const Mongodb = async () => {
+    try {
+        await mongoose.connect(mongoURL);
+        console.log("Database connected successfully");
+    } catch (err) {
+        console.error("Failed to connect to the database", err);
+    }
+}
+
+module.exports = Mongodb;
+
