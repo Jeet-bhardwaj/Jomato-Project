cd1bf5114e36fdaf63dfd1997fb4a21d03ea6b4f
Jeet-bhardwaj <bhardwajjeet409@gmail.com>
2024-10-16 20:43:48 +0530

    Never give up

--- src/Screen/Home.jsx
+++ src/Screen/Home.jsx
@@ -6,36 +6,48 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const Home = () => {
-const [foodCat  , setFoodCat] = useState([]);
-const [foodItem  , setFoodItem] = useState([]);
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
 
-const loadData = async () =>{
-  let response  = await fetch("http://localhost:5000/api/foodData",{
-    method:"POST",
-    headers:{
-      'Content-Type': 'application/json'
-    }
-  })
-  response = await response.json();
-  console.log(response[0],response[1 ]);
-}
+  const loadData = async () => {
+    let response = await fetch("http://localhost:5000/api/foodData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    response = await response.json();
+    console.log(response[0], response[1]);
+    
+    setFoodCat(response[1]);
+    setFoodItem(response[0]);
+  };
 
-
-useEffect(()=>{
-  loadData();
-}, [])
+  useEffect(() => {
+    loadData();
+  }, []);
+  
 
   return (
-    <>
-      <Navbar />
+    <div>
       <div>
-      <Carausal />
+        <Navbar />
       </div>
-      <div className="">
+      <div>
+        <Carausal />
+      </div>
+      <div className="container">
+        {foodCat.length > 0 ? (
+          foodCat.map((data) => {
+            return <div key={data._id} className="fs-3 m-3" >{data.CategoryName}</div>;
+          })
+        ) : (
+          <div></div>
+        )}
         <Card />
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
