0558c643c60fc9e9c6e5cdafc6697fb99830d3a0
Jeet-bhardwaj <bhardwajjeet409@gmail.com>
2024-10-25 00:12:44 +0530

    reset

--- src/Screen/Home.jsx
+++ src/Screen/Home.jsx
@@ -2,25 +2,26 @@ import Navbar from "../Componet/Navbar";
 import Footer from "../Componet/Footer";
 import Card from "../Componet/Card";
 import Carausal from "../Componet/Carausal";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const Home = () => {
      const [foodCat, setFoodCat] = useState([]);
      const [foodItem, setFoodItem] = useState([]);
 
      const loadData = async () => {
-          let response = await fetch("http://localhost:5000/api/foodData", {
-               method: "POST",
-               headers: {
-                    "Content-Type": "application/json",
-               },
-          });
-          response = await response.json();
-          console.log(response[0], response[1]);
-
-          setFoodCat(response[1]);
-          setFoodItem(response[0]);
+          try {
+               let response = await fetch("http://localhost:5000/api/foodData", {
+                    method: "POST",
+                    headers: {
+                         "Content-Type": "application/json",
+                    },
+               });
+               let data = await response.json();
+               setFoodCat(data[1]); // Assuming categories are at index 1
+               setFoodItem(data[0]); // Assuming items are at index 0
+          } catch (error) {
+               console.error("Error fetching food data:", error);
+          }
      };
 
      useEffect(() => {
@@ -38,19 +39,30 @@ const Home = () => {
                </div>
 
                <div className="container">
+                    {/* Checking if foodCat has categories */}
                     {foodCat.length > 0 ? (
-                         foodCat.map((data) => {
-                              return (
-                                   <div key={data._id} className="fs-3 m-3">
-                                        {data.CategoryName}
-                                   </div>
-                              );
-                         })
+                         foodCat.map((category) => (
+                              <div key={category._id}>
+                                   <div className="fs-3 m-3">{category.CategoryName}</div>
+                                   <hr />
+                                   {/* Filtering items by category name */}
+                                   {foodItem.length > 0 ? (
+                                        foodItem
+                                             .filter((item) => item.CategoryName === category.CategoryName)
+                                             .map((filteredItem) => (
+                                                  <div key={filteredItem._id}>
+                                                       {/* Passing the filtered item as props to the Card component */}
+                                                       <Card foodItem={filteredItem} />
+                                                  </div>
+                                             ))
+                                   ) : (
+                                        <div>No items found for this category</div>
+                                   )}
+                              </div>
+                         ))
                     ) : (
-                         <div></div>
+                         <div>Loading categories...</div>
                     )}
-
-                    <Card />
                </div>
 
                <Footer />
