c6932329de9d5be1efe0abca61e95eb9f4c4a84e
Jeet-bhardwaj <bhardwajjeet409@gmail.com>
2024-10-27 14:00:53 +0530

    working on Navbar

--- src/Componet/Navbar.jsx
+++ src/Componet/Navbar.jsx
@@ -1,57 +1,95 @@
-import { Link } from "react-router-dom";
-// import Login from "../Screen/Login";
+import { Link, useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+    const navigate = useNavigate();
+    const handilOnClick = () => {
+        localStorage.removeItem("authToken");
+        navigate("/login");
+    };
+    const isAuthenticated = Boolean(localStorage.getItem("authToken"));
+
     return (
-        <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-                <div className="container-fluid">
-                    <Link className="navbar-brand fs-1 fst-italic" to="#">
-                        Zwigato
-                    </Link>
-                    <button
-                        className="navbar-toggler "
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target="#navbarNav"
-                        aria-controls="navbarNav"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation"
-                    >
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link
-                                    className="nav-link active"
-                                    aria-current="page"
-                                    to="#"
-                                >
-                                    Home
-                                </Link>
-                            </li>
-                        </ul>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+            <div className="container-fluid">
+                <Link className="navbar-brand fs-1 fst-italic mx-3 " to="/">
+                    Zwigato
+                </Link>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav me-auto mb-2">
+                        <li className="nav-item">
+                            <Link className="nav-link active fs-5" to="/">
+                                Home
+                            </Link>
+                        </li>
+                        {isAuthenticated && (
+                            <>
+                                <li className="nav-item">
+                                    <Link
+                                        className="nav-link active fs-5"
+                                        to="/my-order"
+                                    >
+                                        My Order
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link
+                                        className="nav-link active fs-5"
+                                        to="/"
+                                    >
+                                        Account
+                                    </Link>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                    {isAuthenticated ? (
+                        <div>
+                            <Link
+                                className="btn bg-white text-success mx-1"
+                                to=""
+                            >
+                                mycart
+                            </Link>
+                            <Link
+                                className="btn bg-white text-success mx-1"
+                                to=""
+                                onClick={handilOnClick}
+                            >
+                                Logout
+                            </Link>
+                        </div>
+                    ) : (
                         <div className="d-flex">
                             <Link
-                                className="nav-link "
-                                aria-current="page"
+                                className="btn bg-white text-success mx-1"
                                 to="/login"
                             >
                                 Login
                             </Link>
-
                             <Link
-                                className="nav-link "
-                                aria-current="page"
+                                className="btn bg-white text-success mx-1"
                                 to="/signup"
                             >
                                 SignUp
                             </Link>
                         </div>
-                    </div>
+                    )}
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
     );
 };
+
 export default Navbar;
